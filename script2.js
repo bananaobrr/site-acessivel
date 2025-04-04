521b895a0591c506513b43700c7895746835e0c9
bananaobrr <142402700+bananaobrr@users.noreply.github.com>
2025-04-04 07:46:22 -0300

    vv

--- script2.js
+++ script2.js
@@ -6,8 +6,11 @@ const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
     })
     let tamanhoAtualFonte = 1;
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
